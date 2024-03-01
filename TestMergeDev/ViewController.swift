0b1469b1bb6ede7816494affed6ec6382d279d43
akalnik93 <alekseikalnik@gmail.com>
2024-03-01 19:25:48 +0300

    initial project

---
+++ TestMergeDev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMergeDev
+//
+//  Created by Алексей Калнык on 01.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+}
